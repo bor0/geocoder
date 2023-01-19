d568d80bd62bf8bde168e6edc2c5bac7c9cb73d0
Boro Sitnikovski <buritomath@gmail.com>
2023-01-19 18:14:20 +0100

    Update data build for php

--- build-php-dataset.php
+++ build-php-dataset.php
@@ -3,12 +3,12 @@ if ( ! file_exists( 'dataset.json' ) ) {
 	exit( "Filename dataset.json doesn't exist. Run build-dataset.php first.\n" );
 }
 
-$x = file_get_contents( 'dataset.json' );
-$x = json_decode( $x );
+$data = file_get_contents( 'dataset.json' );
+$data = json_decode( $data );
 
 $arr = '<?php' . PHP_EOL . 'return array(' . PHP_EOL;
-foreach ( $x as $foo ) {
-	$arr .= sprintf( '  array( "%s", "%s", "%s" ),', $foo->name, $foo->lat, $foo->lng ) . PHP_EOL;
+foreach ( $data as $entry ) {
+	$arr .= sprintf( '  array( "%s", "%s", "%s", "%s" ),', $entry->city, $entry->country, $entry->lat, $entry->lng ) . PHP_EOL;
 }
 $arr .= ');';
 
