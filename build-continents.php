b20bbe9f33765a738b1b293842d54d0409e51298
Boro Sitnikovski <buritomath@gmail.com>
2023-01-25 10:00:19 +0100

    Add continents info and remove unnecessary php dataset

---
+++ build-continents.php
@@ -0,0 +1,22 @@
+<?php
+$countries = json_decode( file_get_contents( 'https://raw.githubusercontent.com/annexare/Countries/master/data/countries.json' ), true );
+
+$data = array();
+foreach ( $countries as $country ) {
+	$continent = $country['continent'];
+	if ( in_array( $continent, array( 'EU', 'AF', 'AN' ) ) ) {
+		$continent = 'Europe - Africa';
+	}
+	if ( in_array( $continent, array( 'SA', 'NA' ) ) ) {
+		$continent = 'Americas';
+	}
+	if ( in_array( $continent, array( 'AS', 'OC' ) ) ) {
+		$continent = 'Asia - Pacific';
+	}
+	if ( empty( $data[ $continent ] ) ) {
+		$data[ $continent ] = array();
+	}
+	$data[ $continent ][] = $country['name'];
+}
+
+file_put_contents( 'continents.json', json_encode( $data, JSON_UNESCAPED_UNICODE ) );
