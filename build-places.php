aacf9f3884ff0c8e5dae05a64f480a5b72c77ce0
Boro Sitnikovski <buritomath@gmail.com>
2023-02-01 13:45:41 +0100

    Add states to the dataset

--- build-places.php
+++ build-places.php
@@ -1,16 +1,19 @@
 <?php
-// This script unifies countries.json and cities.json into a single, easy to use file
-$countries = json_decode( file_get_contents( 'https://raw.githubusercontent.com/annexare/Countries/master/data/countries.json' ), true );
-$cities    = json_decode( file_get_contents( 'https://raw.githubusercontent.com/lutangar/cities.json/master/cities.json' ), true );
+printf( 'Fetching cities.json...' . PHP_EOL );
+
+$cities = json_decode( file_get_contents( 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/cities.json' ), true );
+
+printf( 'Processing data...' . PHP_EOL );
 
 $data = array();
 
 foreach ( $cities as $city ) {
 	$data[] = array(
 		'city'    => $city['name'],
-		'country' => $countries[ $city['country'] ]['name'],
-		'lat'     => $city['lat'],
-		'lng'     => $city['lng'],
+		'country' => $city['country_name'],
+		'state'   => $city['state_name'],
+		'lat'     => $city['latitude'],
+		'lng'     => $city['longitude'],
 	);
 }
 
