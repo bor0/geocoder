816ac50a0220c50067381f0bda6cccef42ef002c
Boro Sitnikovski <buritomath@gmail.com>
2023-02-06 16:55:21 +0100

    Add some printfs

--- build-continents.php
+++ build-continents.php
@@ -1,7 +1,12 @@
 <?php
+printf( 'Fetching countries.json...' . PHP_EOL );
+
 $countries = json_decode( file_get_contents( 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries.json' ), true );
 
+printf( 'Processing data...' . PHP_EOL );
+
 $data = array();
+
 foreach ( $countries as $country ) {
 	$continent = $country['region'];
 
@@ -16,6 +21,7 @@ foreach ( $countries as $country ) {
 	} elseif ( in_array( $continent, array( 'Asia', 'Oceania' ) ) ) {
 		$continent = 'Asia - Pacific';
 	} else {
+		printf( "Warning! Unspecified continent '%s' for '%s'" . PHP_EOL, $continent, $country['name'] );
 		continue;
 	}
 
